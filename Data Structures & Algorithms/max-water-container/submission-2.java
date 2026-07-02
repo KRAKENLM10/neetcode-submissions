125a7bb6cb270d62a134da49799a5345a52d9bbf
KRAKENLM10 <cristianoronaldoadvance123456@gmail.com>
2026-07-03 00:10:28 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int maxvol = 0;
+        int vol = 0;
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i + 1; j < heights.length; j++) {
+                if (heights[i] < heights[j]) {
+                    vol = heights[i] * (j - i);
+                } 
+                else if (heights[i] >= heights[j]) {
+                    vol = heights[j] * (j - i);
+                }
+                if (maxvol < vol) {
+                maxvol = vol;
+            }
+            }
+
+           
+        }
+        return maxvol;
+    }
+}
