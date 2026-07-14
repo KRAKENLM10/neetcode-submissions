132ca9cf7d2340e2f83d8fe07a47544b19a3aa5e
KRAK3N <cristianoronaldoadvance123456@gmail.com>
2026-07-14 15:22:17 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        Map<Character, Character> map= new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(!stack.isEmpty()&&map.get(s.charAt(i))==stack.peek()){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.isEmpty();
+    }
+}
